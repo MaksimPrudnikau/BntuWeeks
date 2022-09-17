81e6ca19714fb9244a4c8e1d2462e5e8a5aa24e5
Maksim Prudnikau <maksimpruidnikau@gmail.com>
2022-09-17 11:34:43 +0300

    edit commands name, add tomorrow, add past date validate and prepare to heroku deploy

--- calendar.js
+++ calendar.js
@@ -22,21 +22,34 @@ getWeek = (future) => {
 const bot = new Telegraf('5766718501:AAGaJVPisoD1tWWCe0gQVhqPZ9NM9LkqiUU');
 const calendar = new Calendar(bot);
 
-bot.command('week', (ctx) => {
+bot.command('today', (ctx) => {
     const future = new Date();
     
     const week = getWeek(future)
     ctx.reply(`${week}`)
 });
 
+bot.command('tomorrow', (ctx) => {
+    let tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    
+    const week = getWeek(tomorrow)
+    ctx.reply(`${week}`)
+});
+
 calendar.setDateListener((ctx, date) => {
     const future = new Date(date);
+    const today = new Date();
+    if (future.getDate() < today.getDate()){
+        ctx.reply(`Вы выбрали дату, которая уже прошла. Сегодня ${today.toLocaleDateString('ru-RU')}, выбрано ${future.toLocaleDateString('ru-RU')}`)
+        return;
+    }
+    
     const week = getWeek(future)
     const localeDate = future.toLocaleDateString('ru-RU')
     ctx.reply(`На момент ${localeDate} номер недели будет ${week}`)
 });
 
 bot.command("will", context => context.reply("Выбери дату", calendar.getCalendar()));
-
 
 bot.launch()
